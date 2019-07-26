cb5dc33adb8d499dfd98206ff293b004501168e3
Nicholas <nicholasallensharp539@outlook.com>
2019-07-26 16:39:49 -0700

    Initial Commit

---
+++ ConflictingEvents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConflictingEvents
+//
+//  Created by Nicholas Sharp on 7/26/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
