ee4a2968362a318d26ba665696211d21d8a06a8e
Nicholas Sharp <naas8905@gmail.com>
2020-06-03 15:08:54 -0700

    Initial commit

---
+++ ConflictingEvents/EventStructs.swift
@@ -0,0 +1,14 @@
+//
+//  EventStructs.swift
+//  ConflictingEvents
+//
+//  Created by Nicholas Sharp on 7/26/19.
+//
+
+import Foundation
+
+struct Events: Decodable {
+  var title: String
+  var start: String
+  var end: String
+}
--- ConflictingEvents/ViewController.swift
+++ ConflictingEvents/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  struct Events : Decodable {
+    let title : String
+    let start : URL
+    let end : String
+  }
+  
+  var events = [Events]()
+  
+  func parseEvents() {
+    let url = Bundle.main.url(forResource:"mock", withExtension: "json")!
+    let jsonData = try! Data(contentsOf: url)
+    self.events = try! JSONDecoder().decode([Events].self, from: jsonData)
+    self.tableView.reloadData()
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    parseEvents()
   }
-
-
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+  {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+    
+    var dict = Array[indexPath.row]
+    cell.lbl1.text = dict["title"] as? String
+    cell.lbl2.text = dict["start"] as? String
+    cell.lbl3.text = dict["end"] as? String
+ 
+ 
+    let imageUrl:URL = URL(string: imageUrlString as! String)!
+    let imageData:NSData = NSData(contentsOf: imageUrl)!
+    cell.img.image = UIImage(data: imageData as Data)
+    
+    
+    
+    return cell
+  }
 }
+
+
 
